58b11ff59ebf03c5f94a3c600ef8f3e7e5a95631
nuhaniiy <nurulhndyni@gmail.com>
2024-09-19 10:25:31 +0700

    tugas iteraction

---
+++ cypress/e2e/0-personal-learn/iteracting.test.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Browser Action', () => {
+    
+    it('Should load books website', () => {
+        cy.visit('https://books.toscrape.com/index.html', {timeout: 10000})
+        cy.url().should('include', 'index.html')
+
+        
+       // it('should click on Travel Category', () => {
+            cy.get('a').contains('Travel').click()
+            cy.get('h1').contains('Travel') 
+       // });
+    });
+});
